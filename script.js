603d3688aa2ff8c88a3ed3df7c765c503a3a0f15
ana_l_amaral <ana_l_amaral@estudante.sesisenai.org.br>
2025-03-20 09:12:37 -0300

    Add funcao com parametros

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 //Variáveis
 //let numero = prompt('Digite seu número:');
-let numer1 = prompt('Digite seu número:');
-let numer2 = prompt('Digite seu número:');
+//let numer1 = parseFloat(prompt('Digite seu número:'));
+//let numer2 = parseFloat(prompt('Digite seu número:'));
 
 //Funções
-    function verificarPar() {
-        
-        if ( numero % 2 === 0) {
-            console.log (numero + ' é um número par!');
-        } else {
-            console.log ('Esse número é ímpar!');
-        }
-    }
+function verificarPar() {
 
-    function calcular() {
-
-        console.log( numer1 + numer2);
-        console.log( numer1 - numer2);
-        console.log( numer1 * numer2);
-        console.log( numer1 / numer2);      
+    if (numero % 2 === 0) {
+        console.log(numero + ' é um número par!');
+    } else {
+        console.log('Esse número é ímpar!');
     }
+}
+
+function calcular(numer1,numer2) {
+
+    console.log(numer1 + numer2);
+    console.log(numer1 - numer2);
+    console.log(numer1 * numer2);
+    if ( numer2 === 0 ) {
+        console.log(' não é possível ');
+    } else {
+        console.log(numer1 / numer2);
+    }
+    
+}
 
 //Chamando Função
 //verificarPar();
-calcular();
+//calcular(numer1,numer2);
 
 
