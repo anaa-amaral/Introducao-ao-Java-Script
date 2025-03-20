646b972cccebc09fe5050849baddb43ced9cc0da
ana_l_amaral <ana_l_amaral@estudante.sesisenai.org.br>
2025-03-20 09:01:04 -0300

    Add funcao calcular

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 //Variáveis
-let numero = 10;
+//let numero = prompt('Digite seu número:');
+let numer1 = prompt('Digite seu número:');
+let numer2 = prompt('Digite seu número:');
 
 //Funções
     function verificarPar() {
-        console.log(numero);
+        
+        if ( numero % 2 === 0) {
+            console.log (numero + ' é um número par!');
+        } else {
+            console.log ('Esse número é ímpar!');
+        }
+    }
+
+    function calcular() {
+
+        console.log( numer1 + numer2);
+        console.log( numer1 - numer2);
+        console.log( numer1 * numer2);
+        console.log( numer1 / numer2);      
     }
 
 //Chamando Função
-    verificarPar();
+//verificarPar();
+calcular();
 
-//Estrutura de controle
-if ( numero % 2 === 0) {
-    console.log ('Esse número é par!');
-} else {
-    console.log ('Esse número é ímpar!');
-}
+
