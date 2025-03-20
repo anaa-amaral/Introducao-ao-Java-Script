c9bf9b1791b374b1f2a0f1f3ad32270609fc70ee
ana_l_amaral <ana_l_amaral@estudante.sesisenai.org.br>
2025-03-20 08:32:22 -0300

    Add estrutura de controle

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 //Variáveis
+let numero = 10;
 
 //Funções
-function verificarPar()
+    function verificarPar() {
+        console.log(numero);
+    }
+
+//Chamando Função
+    verificarPar();
+
+//Estrutura de controle
+if ( numero % 2 === 0) {
+    console.log ('Esse número é par!');
+} else {
+    console.log ('Esse número é ímpar!');
+}
